14ce9f7424acadbd850961f79d75483f1de49bad
Walter <walteraandrade@gmail.com>
2020-04-08 16:56:25 -0300

    Exercícios de hoje bastante incompletos

---
+++ semana4/objetos/blog/script.js
@@ -0,0 +1,13 @@
+const autor = document.getElementById("nome").value;
+const titulo = document.getElementById("titulo").value;
+const tags = document.getElementById("tags").value;
+const post = document.getElementById("post").value;
+
+// const postCompleto = {
+//     autor: autor,
+//     titulo: titulo,
+//     tags: tags,
+//     post: post
+// }
+
+
