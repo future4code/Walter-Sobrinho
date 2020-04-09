ba72416da70de52f25b79f8a981dcc3303f0741b
Walter <walteraandrade@gmail.com>
2020-04-08 21:25:09 -0300

    Atividade com as funcionalidades normais, mas sem estilização.

--- semana4/objetos/blog/script.js
+++ semana4/objetos/blog/script.js
@@ -1,13 +1,24 @@
-const autor = document.getElementById("nome").value;
-const titulo = document.getElementById("titulo").value;
-const tags = document.getElementById("tags").value;
-const post = document.getElementById("post").value;
+const autor = document.getElementById("nome");
+const titulo = document.getElementById("titulo");
+const tags = document.getElementById("tags");
+const post = document.getElementById("post");
 
-// const postCompleto = {
-//     autor: autor,
-//     titulo: titulo,
-//     tags: tags,
-//     post: post
-// }
+const postCompleto = {
+    autor: autor,
+    titulo: titulo,
+    tags: tags,
+    post: post
+}
+
+function botaoPost(ev) {
+    const postArea = document.getElementById("post-area");
+    postArea.innerHTML += "<p>" + postCompleto.post.value + "</p>"
+    let meuArray = []
+    meuArray.push(postCompleto.autor.value)
+    meuArray.push(postCompleto.titulo.value)
+    meuArray.push(postCompleto.tags.value)
+    meuArray.push(postCompleto.post.value)
+    console.log(meuArray)
+}
 
 
