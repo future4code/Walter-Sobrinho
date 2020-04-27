827fee848f3e54ef4b667129aa4528879182f924
Walter <walteraandrade@gmail.com>
2020-04-27 15:56:35 -0300

    modificações no react da última semana e arquivo do labefy

---
+++ semana6/semana6-aula22/lista-tarefas/boiler-plate/build/precache-manifest.4e157691c196efe689ec6af67491e452.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca6a2059843ea5fc3c709055e7c64aa9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aac0d988e7d3dd914b70",
+    "url": "/static/css/main.676c5720.chunk.css"
+  },
+  {
+    "revision": "d70d575afb58ad9c703f",
+    "url": "/static/js/2.adf6ff92.chunk.js"
+  },
+  {
+    "revision": "f5a66d6c0f7e73552f3af9f6e9caf8d7",
+    "url": "/static/js/2.adf6ff92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aac0d988e7d3dd914b70",
+    "url": "/static/js/main.cd966484.chunk.js"
+  },
+  {
+    "revision": "e5f8bc95d36f0d17637a",
+    "url": "/static/js/runtime-main.16f10ae7.js"
+  }
+]);
